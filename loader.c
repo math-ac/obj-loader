3cb4a87c9aa98cbe199a4b4b50e32580b7e06d5e
Matheus Constancio <ma.constancio@protonmail.com>
2021-09-19 20:22:20 -0300

    Temporary fix for the light

--- loader.c
+++ loader.c
@@ -44,6 +44,14 @@ void init()
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glEnable(GL_SMOOTH);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
+    //glCullFace(GL_FRONT);
+    glFrontFace(GL_CCW);
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LESS);
 }
 
 void display()
